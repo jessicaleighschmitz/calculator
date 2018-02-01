9fdab4d760e09f2986d06a6af38e2818d858830a
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-02-01 01:32:47 -0800

    add index.html, README.md, and script and link tags linking the js and css file

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+return number1 + number2;
+};
+
+var number1 = parseInt(prompt('Enter in a number:'));
+var number2 = parseInt(prompt('Enter in another number:'));
+
+alert(add(number1, number2));
